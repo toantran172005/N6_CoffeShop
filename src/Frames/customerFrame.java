690bee0e2a30092380f964713e182edc5d614dc4
toantran172005 <toantran172005@gmail.com>
2025-04-27 16:41:42 +0700

    changed Word

--- src/Frames/customerFrame.java
+++ src/Frames/customerFrame.java
@@ -34,7 +34,6 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
